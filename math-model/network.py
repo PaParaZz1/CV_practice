726918233fb6d11a4e2f7053aa344783cd30d483
PaParaZz1 <niuyazhe@buaa.edu.cn>
2018-12-25 21:03:08 +0800

    add math model our own network

---
+++ math-model/network.py
@@ -0,0 +1,45 @@
+import torch
+import torch.nn as nn
+from modules import ConvBlockSequential
+
+
+class SFNet(nn.Module):
+    def __init__(self, input_dim, node_list=None):
+        super(SFNet, self).__init__()
+        if node_list is None:
+            self.node_list = [128, 64, 32]
+        else:
+            self.node_list = node_list
+
+        self.layer_number = len(node_list)
+        self.activation = nn.Sigmoid()
+        self.use_bn = False
+
+        network = []
+        layer_input_dim = input_dim
+        for i in range(self.layer_number):
+            network.append(ConvBlockSequential(in_channels=layer_input_dim,
+                                               out_channels=node_list[i],
+                                               kernel_size=1,
+                                               init_type="xavier",
+                                               activation=self.activation,
+                                               use_batchnorm=self.use_bn))
+            layer_input_dim = node_list[i]
+        self.main = nn.Sequential(*network)
+        self.maxpool = nn.MaxPool()
+        self.fc = nn.Linear(self.node_list[-1], 2)
+
+    def forward(self, x):
+        x = self.main(x)
+        x = self.maxpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+if __name__ == "__main__":
+    net = SFNet.cuda()
+    inputs = torch.randn(8, 1, ).cuda()
+    out = net(inputs)
+    print(out.shape)
+    print(out)
