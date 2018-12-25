0ca55d5fd3cbd6a611a33ac4c65cde28caa3fb49
niuyazhe <niuyazhe@sensetime.com>
2018-12-26 01:39:53 +0800

    add dataset

--- math-model/dataset.py
+++ math-model/dataset.py
@@ -1,7 +1,7 @@
 import os
 import random
 import numpy as np
-#mport torch
+import torch
 from torch.utils.data import Dataset
 
 
@@ -22,15 +22,55 @@ def divide():
 
 
 class ModelsimDataset(Dataset):
-    def __init__(self):
-        self.a = a
+    def __init__(self, root, file_list):
+        self.root = root
 
-    def __len(self):
-        return 0
+        self.feature = []
+        self.predict = []
+        with open(file_list, 'r') as f:
+            self.file_list = f.readlines()
+        count = 0
+        for item in self.file_list:
+            print(count)
+            subject = item.split(',')[0]
+            other = item.split(',')[1]
+            with open(root + subject, 'r') as f_subject:
+                subject = f_subject.readlines()
+            with open(root + other[:-1], 'r') as f_other:
+                other = f_other.readlines()
+            print(len(subject))
+            print(len(other))
+            assert(len(subject) == len(other))
+            for i in range(len(subject) - 1):
+                tmp = []
+                for j in subject[i].split(','):
+                    tmp.append(float(j))
+                for j in other[i].split(','):
+                    tmp.append(float(j))
+                self.feature.append(tmp)
+                # label
+                split_result = subject[i + 1].split(',')
+                self.predict.append([float(split_result[0]), float(split_result[1])])
+
+        self.feature = np.array(self.feature)
+        self.predict = np.array(self.predict)
+
+    def __len__(self):
+        return self.feature.shape[0]
 
     def __getitem(self, index):
-        return 0
+        feature = self.feature[index]
+        feature = torch.from_numpy(feature).float()
+        predict = self.predict[index]
+        predict = torch.from_numpy(predict).float()
+        return feature, predict
 
 
 if __name__ == "__main__":
-    divide()
+    dataset = ModelsimDataset(root='./data/a/', file_list='test_a.txt')
+    feature, predict = next(enumerate(dataset))
+    print(feature.shape)
+    print(predict.shape)
+    print(predict)
+    print(feature)
+    print(len(dataset))
